bac2c3d8b2b8fab9276322efe42d5b6c4ce4f5e6
GustavoGomesDias <treeelevengg@gmail.com>
2021-04-23 00:00:33 -0300

    Aula 336 - Estrutura básica

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+
+const http = require('http').createServer(app);
+
+const io = require('socket.io')(http);
+
+io.on("connection", socket => {
+    socket.on("disconnect", () => {
+        console.log("X desconectou: " + socket.id);
+    });
+    
+});
+
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+    res.render("index");
+});
+
+http.listen(3000, () => {
+    console.log("App rodoando");
+});
